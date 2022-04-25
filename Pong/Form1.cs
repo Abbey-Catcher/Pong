8022275d86c9e235518a27d81c7015aab7b83883
abigcatc683 <abigcatc683@SDS-L-763HC63>
2022-04-25 14:43:07 -0400

    Edit

--- Pong/Form1.cs
+++ Pong/Form1.cs
@@ -13,24 +13,31 @@ namespace Pong
     public partial class Pong : Form
     {
 
-        Rectangle player1 = new Rectangle(10, 170, 10, 60);
-        Rectangle player2 = new Rectangle(580, 170, 10, 60);
+        Rectangle player1 = new Rectangle(10, 130, 10, 60);
+        Rectangle player2 = new Rectangle(10, 200, 10, 60);
         Rectangle ball = new Rectangle(295, 195, 10, 10);
 
         int player1Score = 0;
         int player2Score = 0;
+        int playerTurn = 1;
 
         int playerSpeed = 4;
-        int ballXSpeed = 6;
-        int ballYSpeed = -6;
+        int ballXSpeed = 8;
+        int ballYSpeed = 8;
 
         bool wDown = false;
         bool sDown = false;
+        bool aDown = false;
+        bool dDown = false;
         bool upArrowDown = false;
         bool downArrowDown = false;
+        bool rightArrowDown = false;
+        bool leftArrowDown = false;
 
         SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
         SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        Pen whitePen = new Pen(Color.White, 3);
 
         public Pong()
         {
@@ -47,11 +54,23 @@ namespace Pong
                 case Keys.S:
                     sDown = true;
                     break;
+                case Keys.A:
+                    aDown = true;
+                    break;
+                case Keys.D:
+                    dDown = true;
+                    break;
                 case Keys.Up:
                     upArrowDown = true;
                     break;
                 case Keys.Down:
                     downArrowDown = true;
+                    break;
+                case Keys.Left:
+                    leftArrowDown = true;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = true;
                     break;
             }
         }
@@ -66,11 +85,23 @@ namespace Pong
                 case Keys.S:
                     sDown = false;
                     break;
+                case Keys.A:
+                    aDown = false;
+                    break;
+                case Keys.D:
+                    dDown = false;
+                    break;
                 case Keys.Up:
                     upArrowDown = false;
                     break;
                 case Keys.Down:
                     downArrowDown = false;
+                    break;
+                case Keys.Left:
+                    leftArrowDown = false;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = false;
                     break;
             }
         }
@@ -92,6 +123,16 @@ namespace Pong
                 player1.Y += playerSpeed;
             }
 
+            if (aDown == true && player1.X > 0)
+            {
+                player1.X -= playerSpeed;
+            }
+
+            if (dDown == true && player1.X < this.Width - player1.Width)
+            {
+                player1.X += playerSpeed;
+            }
+
             //move player 2 
             if (upArrowDown == true && player2.Y > 0)
             {
@@ -103,48 +144,88 @@ namespace Pong
                 player2.Y += playerSpeed;
             }
 
+            if (leftArrowDown == true && player2.X > 0)
+            {
+                player2.X -= playerSpeed;
+            }
+
+            if (rightArrowDown == true && player2.X < this.Width - player2.Width)
+            {
+                player2.X += playerSpeed;
+            }
+
             //check if ball hit top or bottom wall and change direction if it does 
             if (ball.Y < 0 || ball.Y > this.Height - ball.Height)
             {
                 ballYSpeed *= -1;  // or: ballYSpeed  =  -ballYSpeed; 
             }
 
+            if (ball.X > this.Width - ball.Width)
+            {
+                ballXSpeed *= -1;
+            }
+
             //Check if ball hits either player
             //If it does change the direction and place the ball in front of the player hit 
+
+            if (ball.X <= 0)
+            {
+                if (playerTurn == 1)
+                {
+                    player2Score++;
+                    p2ScoreLabel.Text = $"{player2Score}";
+                }
+                else
+                {
+                    player1Score++;
+                    p1ScoreLabel.Text = $"{player1Score}";
+                }
+
+                ball.X = 295;
+                ball.Y = 195;
+
+                player1.Y = 130;
+                player2.Y = 200;
+                player1.X = 10;
+                player2.X = 10;
+            }
             if (player1.IntersectsWith(ball))
             {
                 ballXSpeed *= -1;
                 ball.X = player1.X + ball.Width;
+                playerTurn = 2;
             }
             else if (player2.IntersectsWith(ball))
             {
                 ballXSpeed *= -1;
-                ball.X = player2.X - ball.Width;
+                ball.X = player2.X + ball.Width;
+                playerTurn = 1;
             }
 
             //check if a player missed the ball and if true add 1 to score of other player  
-            if (ball.X < 0)
-            {
-                player2Score++;
-                p2ScoreLabel.Text = $"{player2Score}";
+            //if (ball.X < 0)
+            //{
+            //    player2Score++;
+            //    p2ScoreLabel.Text = $"{player2Score}";
 
-                ball.X = 295;
-                ball.Y = 195;
+            //    ball.X = 295;
+            //    ball.Y = 195;
 
-                player1.Y = 170;
-                player2.Y = 170;
-            }
-            else if (ball.X > this.Width)
-            {
-                player1Score++;
-                p1ScoreLabel.Text = $"{player1Score}";
+            //    player1.Y = 130;
+            //    player2.Y = 200;
+            //}
+            //else if (ball.X > this.Width)
+            //{
+            //    player1Score++;
+            //    p1ScoreLabel.Text = $"{player1Score}";
 
-                ball.X = 295;
-                ball.Y = 195;
+            //    ball.X = 295;
+            //    ball.Y = 195;
 
-                player1.Y = 170;
-                player2.Y = 170;
-            }
+            //    player1.Y = 130;
+            //    player2.Y = 200;
+            //}
+
 
             // check score and stop game if either player is at 3 
             if (player1Score == 3)
@@ -166,8 +247,18 @@ namespace Pong
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             e.Graphics.FillRectangle(blueBrush, player1);
-            e.Graphics.FillRectangle(blueBrush, player2);
+            e.Graphics.FillRectangle(redBrush, player2);
             e.Graphics.FillRectangle(whiteBrush, ball);
+
+            if (playerTurn == 1)
+            {
+                e.Graphics.DrawRectangle(whitePen, player1);
+            }
+
+            if (playerTurn == 2)
+            {
+                e.Graphics.DrawRectangle(whitePen, player2);
+            }
         }
     }
 }
